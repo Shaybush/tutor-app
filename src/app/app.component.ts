729ece59f8093e0a26af768f9ad2ca02c872eedb
Shay <shaybush93@gmail.com>
2023-02-22 12:44:40 +0200

    style a little bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation:ViewEncapsulation.None
 })
 export class AppComponent implements OnInit{
   loading: boolean = true;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBIiIibS7FV47DkWOcjJFfcswpgXgo7l80",
+    authDomain: "private-tutor-mangement.firebaseapp.com",
+    projectId: "private-tutor-mangement",
+    storageBucket: "private-tutor-mangement.appspot.com",
+    messagingSenderId: "122683559821",
+    appId: "1:122683559821:web:46e3aba9cbc62d4c3977c4"
+  }
 };
