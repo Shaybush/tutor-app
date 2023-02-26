9adcd5f670f731b5c67d861c7f4f1dc5b8564356
benshirom <57137329+benshirom@users.noreply.github.com>
2023-02-26 18:07:14 +0200

    add StudentsResolver and loading

--- src/app/views/dashboard/components/dashboard-students/dashboard-students.component.ts
+++ src/app/views/dashboard/components/dashboard-students/dashboard-students.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Student } from 'src/app/core/api/models/students';
 import { StudentsService } from 'src/app/core/api/service/students.service';
@@ -6,21 +7,34 @@ import { StudentsService } from 'src/app/core/api/service/students.service';
 @Component({
   selector: 'app-dashboard-students',
   templateUrl: './dashboard-students.component.html',
-  styleUrls: ['./dashboard-students.component.scss']
+  styleUrls: ['./dashboard-students.component.scss'],
 })
 export class DashboardStudentsComponent implements OnInit {
-  students !: Student[]
-  constructor(private studentServ: StudentsService) { }
+  students!: Student[];
+
+  constructor(
+    
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.studentServ.getStudentList().subscribe(res =>{
-      this.students = res.map(student =>{
-        return{
-          id : student.payload.doc.id,
-          ...student.payload.doc.data() as {}
+    this.route.data.subscribe((res) => {
+      console.log(res['studentsData']);
+      this.students = res['studentsData'].map((student: { payload: { doc: { id: any; data: () => {}; }; }; }) => {
+        return {
+          id: student.payload.doc.id,
+          ...(student.payload.doc.data() as {}),
         } as Student;
-      })
-    })
+      });
+    });
+    // this.studentServ.getStudentList().subscribe(res =>{
+    //   console.log(res)
+    //   this.students = res.map(student =>{
+    //     return{
+    //       id : student.payload.doc.id,
+    //       ...student.payload.doc.data() as {}
+    //     } as Student;
+    //   })
+    // })
   }
-
 }
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/core/api/service/auth.service';
-
+import {
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent{
-  constructor(public authService: AuthService) { }
+  // constructor(public authService: AuthService) { }
+
+  loadingInPage: boolean = true;
+  constructor(public router: Router) {
+    this.router.events.subscribe(ev => {
+      if (ev instanceof NavigationStart) {
+        this.loadingInPage = true;
+      }
+      if (
+        ev instanceof NavigationEnd ||
+        ev instanceof NavigationCancel ||
+        ev instanceof NavigationError
+      ) {
+        this.loadingInPage = false;
+      }
+    });
+  }
 }
