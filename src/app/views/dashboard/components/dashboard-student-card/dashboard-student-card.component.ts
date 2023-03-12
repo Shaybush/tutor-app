acf08b31c0ec3da1695916037bb069380f647d1f
Shay <shaybush93@gmail.com>
2023-03-12 16:42:18 +0200

    card done

--- src/app/views/dashboard/components/dashboard-student-card/dashboard-student-card.component.ts
+++ src/app/views/dashboard/components/dashboard-student-card/dashboard-student-card.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Student } from 'src/app/core/api/models/students';
+import { arrayClassHebrew } from '../../utils/array-util';
 
 @Component({
   selector: 'app-dashboard-student-card',
   templateUrl: './dashboard-student-card.component.html',
-  styleUrls: ['./dashboard-student-card.component.scss']
+  styleUrls: ['./dashboard-student-card.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DashboardStudentCardComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() student !: Student;
+  studentClass!: string;
   ngOnInit(): void {
+    console.log(this.student)
+    this.studentClass = arrayClassHebrew(this.student.group);
   }
-
 }
